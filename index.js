e7481be91a1e5b12647e858e912ccc03459ea7c2
Hans Martin <hansmhank@gmail.com>
2019-08-19 14:11:40 -0700

    Search working

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
+import search from './lib/search.js';
 import SNIPS from './snippet-data.js';
-
-console.log(SNIPS);
-
-const renderSnips = snippets => {
-  const snippetHTML = snippets
-    .map(
-      snippet => `
-    <div class="snip">
-    <div class="text">
-      <h2>${snippet.title}</h2>
-      <p>${snippet.description}</p>
-    </div>
-
-    <pre><code class="${snippet.language}">${he.encode(snippet.code)}</code></pre>
-  </div>`
-    )
-    .join('');
-
-  console.log(snippetHTML);
-
-  const snippetsElem = document.querySelector('#snippets');
-  snippetsElem.innerHTML = snippetHTML;
-};
+import { renderSnips, highlightSnips } from './lib/snippets.js';
 
 renderSnips(SNIPS);
 
-hljs.initHighlightingOnLoad();
+document.getElementById('search-bar').addEventListener('submit', search);
