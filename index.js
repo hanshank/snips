d25900e4f04e096b2e7b7c9150e2df6ad123572d
Hans Martin <hansmhank@gmail.com>
2019-08-21 13:31:12 -0700

    Some styling of main page

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import search from './lib/search.js';
-import SNIPS from './snippet-data.js';
-import { renderSnips, highlightSnips } from './lib/snippets.js';
+// import SNIPS from './snippet-data.js';
+import { renderSnips, fetchSnippets, highlightSnips } from './lib/snippets.js';
 
-renderSnips(SNIPS);
+// fetchSnippets().then(snips => {
+//   renderSnips(snips);
+// });
 
-document.getElementById('search-bar').addEventListener('submit', search);
+async function init() {
+  const snippets = await fetchSnippets();
+  renderSnips(snippets);
+  document.getElementById('search-bar').addEventListener('submit', search);
+}
+
+init();
